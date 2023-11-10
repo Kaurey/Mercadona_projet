3e98b71a3901780b56bb32d1e1b22e65057d8b7b
Corentin Bouet <corentinbouet77120@gmail.com>
2023-11-10 22:33:15 +0100

    Refactor version migration

--- migrations/Version20231109100201.php
+++ migrations/Version20231109100201.php
@@ -21,10 +21,13 @@ final class Version20231109100201 extends AbstractMigration
     {
         // this up() migration is auto-generated, please modify it to your needs
         $this->addSql('CREATE SEQUENCE promotion_id_seq INCREMENT BY 1 MINVALUE 1 START 1');
-        $this->addSql('CREATE TABLE promotion (id INT NOT NULL, start_date DATE NOT NULL, end_date DATE NOT NULL, percentage INT NOT NULL, PRIMARY KEY(id))');
+        $this->addSql('CREATE TABLE promotion (id INT NOT NULL, start_date DATE NOT NULL, end_date DATE NOT NULL, percentage DOUBLE PRECISION NOT NULL, PRIMARY KEY(id))');
         $this->addSql('ALTER TABLE product ADD category_id INT NOT NULL');
         $this->addSql('ALTER TABLE product ADD CONSTRAINT FK_D34A04AD12469DE2 FOREIGN KEY (category_id) REFERENCES category (id) NOT DEFERRABLE INITIALLY IMMEDIATE');
         $this->addSql('CREATE INDEX IDX_D34A04AD12469DE2 ON product (category_id)');
+        $this->addSql('ALTER TABLE promotion ADD product_id INT NOT NULL');
+        $this->addSql('ALTER TABLE promotion ADD CONSTRAINT FK_C11D7DD14584665A FOREIGN KEY (product_id) REFERENCES product (id) NOT DEFERRABLE INITIALLY IMMEDIATE');
+        $this->addSql('CREATE INDEX IDX_C11D7DD14584665A ON promotion (product_id)');
     }
 
     public function down(Schema $schema): void
@@ -32,6 +35,9 @@ final class Version20231109100201 extends AbstractMigration
         // this down() migration is auto-generated, please modify it to your needs
         $this->addSql('CREATE SCHEMA public');
         $this->addSql('DROP SEQUENCE promotion_id_seq CASCADE');
+        $this->addSql('ALTER TABLE promotion DROP CONSTRAINT FK_C11D7DD14584665A');
+        $this->addSql('DROP INDEX IDX_C11D7DD14584665A');
+        $this->addSql('ALTER TABLE promotion DROP product_id');
         $this->addSql('DROP TABLE promotion');
         $this->addSql('ALTER TABLE product DROP CONSTRAINT FK_D34A04AD12469DE2');
         $this->addSql('DROP INDEX IDX_D34A04AD12469DE2');
